41a9a958df94e45f26d4e4d664a694353f4eadb4
PeterPain <yannik.schroeder@gmail.com>
2019-07-07 11:32:50 +0200

    New chessalyzer#experimental integration

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,10 +3,11 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+	constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+	@Get()
+	async analyze(): Promise<any> {
+		const data = await this.appService.analyze();
+		return data;
+	}
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,36 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+	getHello(): string {
+		return 'Hello World!';
+	}
+
+	async analyze() {
+		const Chessalyzer = require('chessalyzer.js');
+		const { Tracker } = Chessalyzer;
+
+		const a = new Tracker.Game();
+		const b = new Tracker.Piece();
+		const c = new Tracker.Tile();
+		let fun = (data, sqrData, loopSqrData) => {
+			let val = 0;
+			const { coords } = sqrData;
+			const { piece } = loopSqrData;
+			if (piece.color !== '') {
+				val =
+					data.tiles[coords[0]][coords[1]][piece.color][piece.name]
+						.movedTo;
+			}
+			return val;
+		};
+		await Chessalyzer.startBatchMultiCore(
+			'./test/lichess_db_standard_rated_2013-12.pgn',
+			[c],
+			{
+				cntGames: 100000
+			}
+		);
+		// generate a heat map for the data of 'a1' based on your evaluation function
+		return Chessalyzer.generateHeatmap(c, 'a1', fun);
+	}
 }
